a7e3ab733559112be7ad389b4d5f503436a486b4
ehumba <behsukiat@gmail.com>
2024-08-25 10:36:38 +0800

    Framerate

--- main.py
+++ main.py
@@ -10,12 +10,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = (0, 0, 0)
 
+    frames = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
         pygame.display.flip()
+        frames.tick(60)
+        dt = frames.tick() / 1000
+
 
 
 
