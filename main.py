554a0f873469a5d6882264b54c9bb346d7ab1123
ehumba <behsukiat@gmail.com>
2024-08-25 09:43:16 +0800

    Start

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+
+if __name__ == "__main__":
+    main()
+
+
+print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
